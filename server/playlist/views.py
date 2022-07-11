c5c0f639019eb853589693aaea2ef3f2d52274ed
jyjyeon <k-j_y@naver.com>
2022-07-11 16:55:37 +0900

    fix: fix errors

--- server/playlist/views.py
+++ server/playlist/views.py
@@ -3,19 +3,39 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from .models import *
 import json
-from rest_framework import generics, status
-from .serializers import SongSerializer
+from rest_framework import generics, status, viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
-class IndexView(generics.CreateAPIView):
-    queryset = Song.objects.all()
-    serializer_class = SongSerializer
-       
-def save(request):
-    queryset=Song.objects.all()
-    reqData=request.data
-    serializer=SongSerializer(data=reqData)
 
-    if serializer.is_valid():
-        serializer.save()
-            #return HttpResponse('success')
+
+@api_view(['POST'])
+def save(request):
+    data = request.data
+
+    new_playlist = Playlist.objects.create(
+        user_id=data['user_id'],
+        name=data['name'],
+        tag=data['tag']
+    )
+    new_playlist.save()
+
+    if 'songs' in data:
+        for s in data['songs']:
+            song_obj = Song.objects.create(id=s['id'],
+                                           title=s['title'],
+                                           artist=s['artist'],
+                                           duration=s['duration'],
+                                           file=s['file'],
+                                           title_album=s['title_album'],
+                                           image_album=s['image_album'])
+            Song.save(song_obj)
+
+            songlist_obj = Songlist.objects.create(
+                playlist_id=5,
+                song_id=s['id']
+            )
+            Songlist.save(songlist_obj)
+
+    return Response("success")
