83ed918265f0a437521fd5ad64396ed50a489173
jyjyeon <k-j_y@naver.com>
2022-07-11 15:48:48 +0900

    chore: account에서 불필요한 model 삭제

--- server/account/models.py
+++ server/account/models.py
@@ -117,18 +117,6 @@ class AuthUserUserPermissions(models.Model):
         unique_together = (('user', 'permission'),)
 
 
-class Category(models.Model):
-    id = models.CharField(primary_key=True, max_length=50)
-    keyword = models.CharField(max_length=50, blank=True, null=True)
-    genre = models.CharField(max_length=50, blank=True, null=True)
-    danceability = models.FloatField(blank=True, null=True)
-    energy = models.FloatField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'category'
-
-
 class DjangoAdminLog(models.Model):
     action_time = models.DateTimeField()
     object_id = models.TextField(blank=True, null=True)
@@ -173,40 +161,3 @@ class DjangoSession(models.Model):
     class Meta:
         managed = False
         db_table = 'django_session'
-
-
-class Playlist(models.Model):
-    id = models.CharField(primary_key=True, max_length=50)
-    user = models.ForeignKey('User', models.DO_NOTHING, blank=True, null=True)
-    name = models.CharField(max_length=50, blank=True, null=True)
-    tag = models.CharField(max_length=50, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'playlist'
-
-
-class Song(models.Model):
-    id = models.CharField(primary_key=True, max_length=50)
-    title = models.CharField(max_length=200, blank=True, null=True)
-    artist = models.CharField(max_length=50, blank=True, null=True)
-    duration = models.IntegerField(blank=True, null=True)
-    file = models.CharField(max_length=500, blank=True, null=True)
-    title_album = models.CharField(max_length=200, blank=True, null=True)
-    image_album = models.CharField(max_length=500, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'song'
-
-
-class Songlist(models.Model):
-    id = models.CharField(primary_key=True, max_length=50)
-    playlist = models.ForeignKey(
-        Playlist, models.DO_NOTHING, blank=True, null=True)
-    song = models.ForeignKey(Song, models.DO_NOTHING, blank=True, null=True)
-    num = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'songlist'
