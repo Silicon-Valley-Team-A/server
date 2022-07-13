ea058f6bd1d13259ee4c07f1f382430927e5388f
hgyuhyeon <hwgyuhyeon@gmail.com>
2022-07-13 15:47:42 +0900

    fix: 모듈 import 에러 수정

--- server/main/views.py
+++ server/main/views.py
@@ -1,9 +1,7 @@
 from email.mime import application
 import json
 from django.http import HttpResponse
-from django.shortcuts import render
-from sympy import content
 
 # Create your views here.
 def main(request):
-    return HttpResponse("Hi there!", content_type=application/json)
+    return HttpResponse("index page")
