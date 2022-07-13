20d0b9beb217755442ab4e23e48f33e235de9f41
choiyounji <yunji71@naver.com>
2022-07-13 15:17:55 +0900

    feat: POST수정

--- server/playlist/views.py
+++ server/playlist/views.py
@@ -6,36 +6,39 @@ import json
 from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.db.models import Max
+import requests
 
 # Create your views here.
 
-
-@api_view(['POST'])
+#@api_view(['POST'])
 def save(request):
-    data = request.data
+    if request.method =="POST":
+    #data = request.data
+        new_playlist = Playlist.objects.create(
+            user_id=request.POST['user_id'],
+            name=request.POST['name'],
+            tag=request.POST['tag']
+        )
+        new_playlist.save()
+        maxid=Playlist.objects.aggregate(Max('id'))
+        #new_playlist.id
 
-    new_playlist = Playlist.objects.create(
-        user_id=data['user_id'],
-        name=data['name'],
-        tag=data['tag']
-    )
-    new_playlist.save()
+        if 'songs' in request.POST:
+            for s in request.POST['songs']:
+                song_obj = Song.objects.create(id=s['id'],
+                                        title=s['title'],
+                                        artist=s['artist'],
+                                        duration=s['duration'],
+                                        file=s['file'],
+                                        title_album=s['title_album'],
+                                        image_album=s['image_album'])
+                Song.save(song_obj)
 
-    if 'songs' in data:
-        for s in data['songs']:
-            song_obj = Song.objects.create(id=s['id'],
-                                           title=s['title'],
-                                           artist=s['artist'],
-                                           duration=s['duration'],
-                                           file=s['file'],
-                                           title_album=s['title_album'],
-                                           image_album=s['image_album'])
-            Song.save(song_obj)
+                songlist_obj = Songlist.objects.create(
+                    playlist_id=maxid['id__max'],
+                    song_id=s['id']
+                )
+                Songlist.save(songlist_obj)
 
-            songlist_obj = Songlist.objects.create(
-                playlist_id=5,
-                song_id=s['id']
-            )
-            Songlist.save(songlist_obj)
-
-    return Response("success")
+        return requests.post(url)
