c949b234821af571451eab738406f92ba3968df3
hgyuhyeon <hwgyuhyeon@gmail.com>
2022-07-15 14:04:11 +0900

    fix: host 주소랑 playlist method 수정

--- server/server/settings.py
+++ server/server/settings.py
@@ -165,7 +165,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ['https://localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 
 CORS_REPLACE_HTTPS_REFERER = True
 
@@ -193,7 +193,7 @@ CORS_ALLOW_HEADERS = [
 ]
 
 CORS_ORIGIN_WHITELIST = (
-    'https://localhost:3000',
+    'http://localhost:3000',
 )
 
 # Default primary key field type
