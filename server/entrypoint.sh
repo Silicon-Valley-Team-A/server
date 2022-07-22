abbdf5b9f89d9221294914e2a62eb057d0b46932
hgyuhyeon <hwgyuhyeon@gmail.com>
2022-07-22 17:30:56 +0900

    fix: CRLF to LF

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 python3 manage.py makemigrations account
-python3 manage.py makemigrations playlist 
-python3 manage.py makemigrations model 
-python3 manage.py migrate 
+
+python3 manage.py makemigrations playlist
+
+python3 manage.py makemigrations model
+
+python3 manage.py migrate
+
 gunicorn --bind 0.0.0.0:8000 server.wsgi:application -t 240
